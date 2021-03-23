6ee8c3063c891f7d66961ad7e6ef1872152bd760
Hatem Moussa <hatemmoussa@192.168.0.101>
2021-03-23 03:14:59 +0200

    inial commit

---
+++ config/firebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBRJA79k0oDpj5ir4guj_fM8ccygH5ENk0",
+    authDomain: "signal-2c3cc.firebaseapp.com",
+    projectId: "signal-2c3cc",
+    storageBucket: "signal-2c3cc.appspot.com",
+    messagingSenderId: "767387574855",
+    appId: "1:767387574855:web:398dc1bb7cf3d555564762"
+  };
