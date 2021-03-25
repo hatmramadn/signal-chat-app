ef95f7aa0eab7319004682a9d31bffd104806496
Hatem Moussa <hatemmoussa@192.168.0.101>
2021-03-25 08:04:49 +0200

    finishing the app

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBRJA79k0oDpj5ir4guj_fM8ccygH5ENk0",
-  authDomain: "signal-2c3cc.firebaseapp.com",
-  projectId: "signal-2c3cc",
-  storageBucket: "signal-2c3cc.appspot.com",
-  messagingSenderId: "767387574855",
-  appId: "1:767387574855:web:398dc1bb7cf3d555564762",
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
 };
 
 let app;
